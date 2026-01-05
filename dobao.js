35008fc14c34f68fb1c3e5dc129211c1edd1619d
MilkWind <2250957906@qq.com>
2026-01-05 16:20:40 +0800

    Refactor inline style application for .chrome70-container elements to enhance responsiveness and manage input field width dynamically

--- dobao.js
+++ dobao.js
@@ -11,19 +11,31 @@
 
 (function () {
     'use strict';
-    /*
-    持续检测第一个直接子元素包含data-target-id="message-box-target-id"属性的所有.chrome70-container元素，
-    将它们的内联样式：--center-content-max-width改为auto，padding: 0 1rem，
-    如果检测到有新的元素，则立即应用。
-    */
+
     const observer = new MutationObserver(() => {
+        /*
+        检测所有的.chrome70-container元素，
+        将它们的内联样式：--center-content-max-width改为auto，padding: 0 1rem，
+        如果是输入框，解除它的最大宽度限制；
+        如果检测到有新的元素，则立即应用。
+        */
         const containers = document.querySelectorAll('.chrome70-container');
+        let isInputSetted = false; // 是否已经设置过输入框的最大宽度限制
         containers.forEach(container => {
-            if (container.querySelector('[data-target-id="message-box-target-id"]')) {
-                container.style.cssText = `
+            container.style.cssText = `
                 --center-content-max-width: auto !important;
                 padding: 0 1rem;
             `;
+
+            // 如果还没有设置过输入框的最大宽度限制，则设置它
+            if (!isInputSetted) {
+                const input = container.querySelector('[class^="inner"]');
+                if (input) {
+                    input.style.cssText = `
+                        max-width: auto;
+                    `;
+                    isInputSetted = true;
+                }
             }
         });
     });
